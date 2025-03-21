582ab59fff1dc05b00633d345a4de0195a49cf4a
Julian Beck <ju-fa-beck@t-online.de>
2025-03-21 21:43:40 +0100

    Initial Commit

---
+++ watermarkremover/watermarkremoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  watermarkremoverApp.swift
+//  watermarkremover
+//
+//  Created by Julian Beck on 21.03.25.
+//
+
+import SwiftUI
+
+@main
+struct watermarkremoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
