ec61a9e75ecab4f7092bde692ba272ada3c666b6
Julian Beck <ju-fa-beck@t-online.de>
2025-03-29 20:47:15 +0100

    update

--- watermarkremover/helpers/Helpers.swift
+++ watermarkremover/helpers/Helpers.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct Helpers {
-    public static let privacyPolicyURL = URL(string: "https://julianbeck.notion.site/Privacy-Policy-for-VirtualTryOn-1be96d29972e80c2980fc188097002ec?pvs=4")!
-    public static let termsOfServiceURL = URL(string: "https://julianbeck.notion.site/VirtualTryOn-1be96d29972e8003a48bd5f6b3652936?pvs=73")!
+    public static let privacyPolicyURL = URL(string: "https://julianbeck.notion.site/Privacy-Policy-for-UNTAG-1c596d29972e800caed7f4e74fbfbd99?pvs=4")!
+    public static let termsOfServiceURL = URL(string: "https://julianbeck.notion.site/Terms-of-Service-for-Untag-1c596d29972e80259815de92b585ee90?pvs=4")!
     public static let supportURL = URL(string: "https://julianbeck.notion.site/15696d29972e80b59185e8aae36dcda0?pvs=105")!
 }
